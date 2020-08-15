d357af664ebde4b6030c2c007a8de1f0e366eab7
2oneweek <dlgkswn885@korea.ac.kr>
2020-08-15 21:16:34 +0900

    dd

--- project/app/models.py
+++ project/app/models.py
@@ -1,14 +1,17 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import User
-
 
 # Create your models here.
 
 
 class Profile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
-    region = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50, blank=True)
+    region = models.CharField(max_length=50, blank=True)
+    building = models.CharField(max_length=50, blank=True)
     isAuth = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username
+        return self.nickname
